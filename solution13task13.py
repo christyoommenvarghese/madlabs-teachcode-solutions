efca43a5780687294ddc13c902d77f4a039610dd
christyoommenvarghese <oommenchristy99@gmail.com>
2017-10-01 02:37:01 +0000

    13Task Submitted

---
+++ solution13task13.py
@@ -0,0 +1,13 @@
+numOne = 0
+numOne += 0
+numTwo = 50
+numTwo += 2
+numThree = 40
+numThree -= 2
+numFour = 20
+numFour *= 2
+numFive = 80
+numFive /= 4.0
+numSix = 100
+numSix %= 10
+print(numOne, numTwo, numThree, numFour, numFive, numSix)
